14787b4ce1fb4ecdf2fc1949220023ee97a31caa
wmafendi <wmafendi@gmail.com>
2026-04-26 20:49:57 +0800

    feat: add DefaultCategories data class

---
+++ app/Data/DefaultCategories.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Data;
+
+class DefaultCategories
+{
+    public const LIST = [
+        ['name' => 'Food & Drinks',    'type' => 'expense'],
+        ['name' => 'Transport',         'type' => 'expense'],
+        ['name' => 'Shopping',          'type' => 'expense'],
+        ['name' => 'Bills & Utilities', 'type' => 'expense'],
+        ['name' => 'Health/Medical',    'type' => 'expense'],
+        ['name' => 'Business',          'type' => 'expense'],
+        ['name' => 'Entertainment',     'type' => 'expense'],
+        ['name' => 'Education',         'type' => 'expense'],
+        ['name' => 'Travel',            'type' => 'expense'],
+        ['name' => 'Donation/Zakat',    'type' => 'expense'],
+        ['name' => 'Household',         'type' => 'expense'],
+        ['name' => 'Remuneration',      'type' => 'expense'],
+        ['name' => 'Other Expense',     'type' => 'expense'],
+        ['name' => 'Salary',            'type' => 'income'],
+        ['name' => 'Freelance',         'type' => 'income'],
+        ['name' => 'Investment',        'type' => 'income'],
+        ['name' => 'Commission',        'type' => 'income'],
+        ['name' => 'Gift',              'type' => 'income'],
+        ['name' => 'Bonus',             'type' => 'income'],
+        ['name' => 'Pension',           'type' => 'income'],
+        ['name' => 'Other Income',      'type' => 'income'],
+    ];
+}
