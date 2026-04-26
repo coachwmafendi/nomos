324f5d6f5983461a132085925b4579767512e9e4
wmafendi <wmafendi@gmail.com>
2026-04-26 21:00:47 +0800

    feat: apply UserCategoryScope and user relationship to Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,16 +2,22 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\UserCategoryScope;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Model;
 
+#[ScopedBy(UserCategoryScope::class)]
 class Category extends Model
 {
-   
-    protected $fillable = ['name', 'type'];
+    protected $fillable = ['user_id', 'name', 'type'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function transactions()
     {
         return $this->hasMany(Transaction::class);
     }
-
 }
